bb11872b1aab65b3f03a5244fc3f31d8825a79de
Yuriy Yurinskiy <yurinskiy@krasmed.ru>
2020-06-05 20:43:45 +0700

    build(composer): разворачивает проект

---
+++ src/Core/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('::index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
